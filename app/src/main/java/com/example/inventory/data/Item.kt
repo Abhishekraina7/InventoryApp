e3fc1d7193829932442793ebca7ffce50e75c8aa
Abhishek Raina <143702712+Abhishekraina7@users.noreply.github.com>
2026-02-24 22:37:07 +0530

    Created the Room Entity/ DB table for storing our inventory items.

--- app/src/main/java/com/example/inventory/data/Item.kt
+++ app/src/main/java/com/example/inventory/data/Item.kt
@@ -1,10 +1,15 @@
 package com.example.inventory.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 
 /**
  * Entity data class represents a single row in the database.
  */
-class Item(
+@Entity(tableName = "items")
+data class Item(
+    @PrimaryKey(autoGenerate = true) //this generates the unique value for every entity instance (every row inserted)
     val id: Int = 0,
     val name: String,
     val price: Double,
