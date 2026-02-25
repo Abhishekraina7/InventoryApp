3495adf051421ec6809826e0681bb4a92bb34287
Abhishek Raina <143702712+Abhishekraina7@users.noreply.github.com>
2026-02-26 00:56:17 +0530

    Created the Repository Interface also with implementation of the Repository Interface.

--- app/src/main/java/com/example/inventory/data/ItemsRepository.kt
+++ app/src/main/java/com/example/inventory/data/ItemsRepository.kt
@@ -1,6 +1,17 @@
 package com.example.inventory.data
 
+import kotlinx.coroutines.flow.Flow
 /**
  * Repository that provides insert, update, delete, and retrieve of [Item] from a given data source.
  */
-interface ItemsRepository
+interface ItemsRepository{
+    fun getAllItemsStream(): Flow<List<Item>>
+
+    fun getItemStream(id: Int): Flow<Item>
+
+    suspend fun insertItem(item: Item)
+
+    suspend fun update(item: Item)
+
+    suspend fun delete(item: Item)
+}
--- app/src/main/java/com/example/inventory/data/OfflineItemsRepository.kt
+++ app/src/main/java/com/example/inventory/data/OfflineItemsRepository.kt
@@ -1,3 +1,25 @@
 package com.example.inventory.data
 
-class OfflineItemsRepository : ItemsRepository
+import kotlinx.coroutines.flow.Flow
+
+class OfflineItemsRepository(private val itemDao: ItemDao) : ItemsRepository {
+    override fun getAllItemsStream(): Flow<List<Item>> {
+        return itemDao.getAllItems()
+    }
+
+    override fun getItemStream(id: Int): Flow<Item> {
+        return itemDao.getItem(id)
+    }
+
+    override suspend fun insertItem(item: Item) {
+        return itemDao.AddItem(item)
+    }
+
+    override suspend fun update(item: Item) {
+        return itemDao.UpdateItem(item)
+    }
+
+    override suspend fun delete(item: Item) {
+        return itemDao.DeleteItem(item)
+    }
+}
